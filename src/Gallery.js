cd004b1770afc66fb7df48a8ea79b972759c1917
Alexey <alexey@wizardondemand.com>
2020-03-05 21:15:54 +0100

    Remove imgix-core-js

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Imgix from 'react-imgix'
 
--- src/HeroImage.js
+++ src/HeroImage.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Background } from 'react-imgix';
-import { buildURL } from "react-imgix";
-var ImgixClient = require('imgix-core-js');
-
-
 
 export const HeroImage = () => <Background src="https://wizardondemand-s3.imgix.net/forest1.jpeg" className="hero-image">
   <h2>Imgix demo</h2>
