ed053f3cf0d4550dff01a58d28b6ccade84bafc0
Chief Wizard <40777040+chief-wizard@users.noreply.github.com>
2020-03-10 20:56:43 +0100

    Fix h2 header

--- src/HeroImage.js
+++ src/HeroImage.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Background } from 'react-imgix';
 
 export const HeroImage = () => <Background src="https://wizardondemand-s3.imgix.net/forest1.jpeg" className="hero-image">
-  <h2>Imgix demo</h2>
+  <h2>imgix demo</h2>
 </Background>
