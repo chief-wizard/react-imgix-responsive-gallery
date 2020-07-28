e8863be5885741af09510d40ba6c45eea94864bd
Chief Wizard <40777040+chief-wizard@users.noreply.github.com>
2020-07-28 10:27:12 +0200

    Update the base URL in HeroImage.js

--- src/HeroImage.js
+++ src/HeroImage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Background } from 'react-imgix';
 
-export const HeroImage = () => <Background src="https://wizardondemand-s3.imgix.net/forest1.jpeg" className="hero-image">
+export const HeroImage = () => <Background src="https://wizard-s3.imgix.net/forest1.jpeg" className="hero-image">
   <h2>imgix demo</h2>
 </Background>
